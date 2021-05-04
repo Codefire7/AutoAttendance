40b8441eee79ff3c19fa12d0c9d9f022f1f64206
lyteral <sabyzmail1@gmail.com>
2021-05-04 22:48:03 +0530

    🛠 Fixed some things
    Removed some unnecessary info.

--- attend_meet.py
+++ attend_meet.py
@@ -2,6 +2,8 @@ import webbrowser
 import pyautogui
 import time
 
+name = input('Enter name : ')
+
 def login(sub):
     profile = "Profile 5"
     chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe --profile-directory="{}" %s'.format(profile)
@@ -10,24 +12,25 @@ def login(sub):
 
 def sub_url(sub):
     if sub.lower() in ['chem','che','chemistry']:
-        return 'https://meet.google.com/sxz-ayna-cya'
+        return 'https://meet.google.com/<id>'
     elif sub.lower() in ['phy','physics']:
-        return 'https://meet.google.com/kuk-urkd-cwj'
+        return 'https://meet.google.com/<id>'
     elif sub.lower() in ['math','maths']:
-        return 'https://meet.google.com/fym-izjk-ymb'
+        return 'https://meet.google.com/<id>'
     elif sub.lower() in ['cs','csc']:
-        return 'https://meet.google.com/gzx-emwq-yxf'
+        return 'https://meet.google.com/<id>'
     elif sub.lower() in ['eng','english']:
-        return 'https://meet.google.com/xdc-mbdy-ini'
+        return 'https://meet.google.com/<id>'
     elif sub.lower() in ['test']:
         return 'https://meet.google.com/ndi-efnv-cxb'
 
 def post_join():
+    global name
     time.sleep(10)
     pyautogui.click(x=1201, y=79, clicks = 2)
     time.sleep(2)
     pyautogui.click(x=1164, y=710, clicks = 2)
-    pyautogui.typewrite(['S','a','i',' ','S','a','b','a','r','i','s','h',' ','1','2','H','2','6','enter'],interval=0.1)
+    pyautogui.typewrite([list(name),'enter'],interval=0.1)
 
 def main():
     sub = input('Enter subject:')
