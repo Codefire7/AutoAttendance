3234ab8a889b50cf5f1e22d61ea2212f3810a3cd
lyteral <sabyzmail1@gmail.com>
2021-05-04 22:43:34 +0530

    ✅ Cleaned up some code
    left some unnecessary details in the code 😅

--- attend_whatsapp.py
+++ attend_whatsapp.py
@@ -3,9 +3,12 @@ import pyautogui
 import schedule
 import time
 
+name = input('Enter your name : ')
+
 def msg():
+    global name
     pyautogui.click(x=546,y=695)
-    pyautogui.typewrite(['S','a','i',' ','S','a','b','a','r','i','s','h',' ','p','r','e','s','e','n','t',' ','m','a',"'",'a','m','enter'],interval=0.1)
+    pyautogui.typewrite([list(name),' ','p','r','e','s','e','n','t','enter'],interval=0.1)
 
 def test():
     pyautogui.click(x=49, y=44)
@@ -13,7 +16,7 @@ def test():
     pyautogui.click(x=1201, y=79, clicks = 2)
     time.sleep(2)
     pyautogui.click(x=1164, y=710, clicks = 2)
-    pyautogui.typewrite(['S','a','i',' ','S','a','b','a','r','i','s','h',' ','1','2','H','2','6','enter'],interval=0.1)
+    pyautogui.typewrite([list(name),' ','1','2','H','2','6','enter'],interval=0.1)
 
 
 schedule.every().day.at("07:30").do(msg)
